407e2cd7858e2ec91aacd5c033f900646e4bdbe5
Rakesh Swain <Rakesh.Swain@Honeywell.com>
2021-09-20 12:50:40 +0530

    inittial

---
+++ src/main/java/com/demo/sitedemo/SitedemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/sitedemo/SitedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.sitedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
